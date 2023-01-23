083d0395415ebd5cdcd5fc955dc3ee4c43608495
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-01-23 12:46:31 +0000

    The stack with comments

--- ASD-Python/Stack.py
+++ ASD-Python/Stack.py
@@ -1,20 +1,26 @@
 class Stack():
 
+    # The constructor of the class
     def __init__(self):
         self.l = []
 
+    # The method Push that append an element at the end
     def Push(self, n):
         self.l.append(n)
 
+    # The method pop that remove the last element
     def Pop(self):
         return self.l.pop()
     
+    # The method Peek that return the last element
     def Peek(self):
         return self.l[-1]
     
+    # The method size that return the size of the the stack
     def Size(self):
         return len(self.l)
     
+    # The method isEmpty that verify if the stack is empty or not
     def isEmpty(self):
         if len(self.l) == 0:
             return True
